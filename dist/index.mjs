cb553f28fb08d98d7deba673b9ab1f1a090b044b
MengLuoRJ <mengluor@gmail.com>
2024-10-04 23:52:50 +0800

    dist: version 0.0.3

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,2 +1,2 @@
-import{join as r}from"path";function t(){if(process.env.USE_SYSTEM_7ZA==="true")return"7z";let e=r(__dirname,"..","bin");return process.platform==="win32"?r(e,"win",process.arch,"7z.exe"):process.platform==="darwin"?r(e,"mac","7zz"):r(e,"linux",process.arch,"7zz")}var s=t(),o=r(__dirname,"7x.sh");export{o as path7x,s as path7z};
+import{join as r}from"path";function t(){if(process.env.USE_SYSTEM_7Z==="true")return"7z";let e=r(__dirname,"..","bin");return process.platform==="win32"?r(e,"win",process.arch,"7z.exe"):process.platform==="darwin"?r(e,"mac","7zz"):r(e,"linux",process.arch,"7zzs")}var s=t(),o=r(__dirname,"7x.sh");export{o as path7x,s as path7z};
 //# sourceMappingURL=index.mjs.map
